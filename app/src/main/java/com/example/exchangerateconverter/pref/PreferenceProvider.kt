3a2203186ff4f9b02d33f7e6fd7ed740b2a1e948
Sukhdip <=>
2021-02-20 19:44:12 -0800

    make the shared preferences dependancies injected by hilt

--- app/src/main/java/com/example/exchangerateconverter/pref/PreferenceProvider.kt
+++ app/src/main/java/com/example/exchangerateconverter/pref/PreferenceProvider.kt
@@ -3,17 +3,19 @@ package com.example.exchangerateconverter.pref
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import javax.inject.Inject
 
 private const val KEY_LAST_FETCH_TIME = "key_last_fetch_time"
 
-class PreferenceProvider(
-    context: Context
+@ActivityRetainedScoped
+class PreferenceProvider @Inject constructor(
+    @ApplicationContext private val context: Context
 ) {
 
-    private val appContext = context.applicationContext
-
     private val preference: SharedPreferences
-        get() = PreferenceManager.getDefaultSharedPreferences(appContext)
+        get() = PreferenceManager.getDefaultSharedPreferences(context)
 
     fun saveLatestFetchTime(fetchedAt: Int) {
         preference.edit().putInt(KEY_LAST_FETCH_TIME, fetchedAt).apply()
--- app/src/main/java/com/example/exchangerateconverter/ui/ExchangeRatesViewModel.kt
+++ app/src/main/java/com/example/exchangerateconverter/ui/ExchangeRatesViewModel.kt
@@ -1,26 +1,22 @@
 package com.example.exchangerateconverter.ui
 
-import android.app.Application
-import android.content.Context
-import android.util.Log
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.exchangerateconverter.models.ExchangeRates
 import com.example.exchangerateconverter.pref.PreferenceProvider
 import com.example.exchangerateconverter.repository.ExchangeRatesRepository
 import com.example.exchangerateconverter.util.Constants.Companion.FETCH_INTERVAL
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.launch
-import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
 class ExchangeRatesViewModel @Inject constructor(
     private val exchangeRatesRepository: ExchangeRatesRepository,
-    @ApplicationContext context: Context
+    private val preferences: PreferenceProvider
 ) : ViewModel() {
-
-    private val preferences = PreferenceProvider(context)
 
     private val _allExchangeRates = exchangeRatesRepository.allExchangeRates
     private val _userCurrencyInput = MutableLiveData<Double>()
